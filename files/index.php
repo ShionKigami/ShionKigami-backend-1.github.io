85bca15b49d3d4f0f4fc03687a8b8aa665fdace0
ShionKigami <steblewets.ant@gmail.com>
2026-04-13 10:06:01 +0300

    Add files via upload

--- files/index.php
+++ files/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Отправляем правильную кодировку.
-header('HTTP/1.1 40 Not Found');
+//header('HTTP/1.1 40 Not Found');
 header('Content-Type: text/html; charset=UTF-8');
 
 // Выводим все полученные через POST параметры.
